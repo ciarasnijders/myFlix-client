0ef097b53b26c42b254ca2009e01dcfd95da1749
Ciara <ciarazsnijders@gmail.com>
2021-11-02 14:15:16 +0100

    create movie-card (jsx and scss files)

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
     render() {
@@ -7,9 +8,25 @@ export class MovieCard extends React.Component {
         const { movie, onMovieClick } = this.props;
         
         return(
-            <div className="movie-card" onClick= {() => { onMovieClick(movie); }}>
-                {movie.title}
-            </div>
-        )
+            <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+    );
   }
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        year: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            bio: PropTypes.string.isRequired,
+            birthYear: PropTypes.string.isRequired,
+            deathYear: PropTypes.string.isRequired
+        }).isRequired,
+        featured:PropTypes.bool,
+        imageURL: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
