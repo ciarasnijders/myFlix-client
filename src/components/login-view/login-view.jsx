b9426c4fc69c234571b61e74471e267b78cd391a
Ciara <ciarazsnijders@gmail.com>
2021-11-02 14:14:48 +0100

    create login-view (jsx and scss files)

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedInHandler(username) */
+    props.onLoggedInHandler(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <button type="registration" onClick={() => props.handleRegistration()}>Sign up</button>
+    </form>
+  );
+}
