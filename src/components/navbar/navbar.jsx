165d6988cc5871285936044976f7d6e17251cda1
Ciara <ciarazsnijders@gmail.com>
2022-01-26 19:59:10 +0100

    create navbar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Navbar,Container, Nav} from 'react-bootstrap';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="images/logo.png"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        WhatFlix movie app
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">List of Movies</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="/users/:username">My Profile</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+};
+
+export default NavBar;
