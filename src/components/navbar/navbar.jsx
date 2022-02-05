bd4e2833480d0f90a56382da7892f917511a0944
Ciara <ciarazsnijders@gmail.com>
2022-02-05 18:44:49 +0100

    add styling to navbar (add images and reposition links)s

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import {Navbar,Container, Nav} from 'react-bootstrap';
+import logo from './assets/logo.png';
+import AppName from './assets/AppName.png';
+import './navbar.scss';
 
 class NavBar extends React.Component {
     render() {
         return (
-            <Navbar collapseOnSelect expand="lg" bg="white" variant="light" fixed="top">
+            <Navbar className="navbar" collapseOnSelect expand="lg" style={{backgroundColor: 'white'}} fixed="top">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand className="name">
                         <img
-                            alt=""
-                            src="images/logo.png"
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
+                            alt="app logo"
+                            src={logo}
+                            width="50px"
+                            height="50px"
+                            className="d-inline-block align-left logo"
                         />{' '}
-                        WhatFlix movie app
+                        <img
+                            alt="app name"
+                            src={AppName}
+                            height="58"
+                            width="200"
+                        />{' '}
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/">Homepage</Nav.Link>
                         </Nav>
                         <Nav>
+                            <Nav.Link href="/">Homepage</Nav.Link>
                             <Nav.Link href="/favorites">My Favorites</Nav.Link>
                             <Nav.Link href="/users/:username">My Profile</Nav.Link>
                         </Nav>
