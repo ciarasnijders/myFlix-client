2d207942e25753e2400eb98aacb74b9ba1da7d0a
Ciara <ciarazsnijders@gmail.com>
2022-02-07 19:37:46 +0100

    add search bar  to movie list (filter) + styling

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import { setFilter } from '../../actions/actions';
+import './visibility-filter-input.scss';
 
 function VisibilityFilterInput(props) {
-    return <Form.Control
+    return <Form.Control className="searchbar"
         onChange={e => props.setFilter(e.target.value)}
         value={props.visibilityFilter}
-        placeholder="filter"
+        placeholder="Search for movie..."
+        variant="light" 
     />;
 }
+
+// const mapStateToProps = ({visibilityFilter}) => ({
+//     visibilityFilter
+// });
+
+// export default connect(mapStateToProps)(VisibilityFilterInput);
 
 export default connect(
     null,
