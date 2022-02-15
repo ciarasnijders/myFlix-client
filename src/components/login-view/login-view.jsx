aec9026720d2d80f40ca81bcd4c4ebff47fa35ad
Ciara <ciarazsnijders@gmail.com>
2022-02-15 18:45:37 +0100

    delete unnecessary code (form imported twice from bootstrap)

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import {Form, Button, Card, Container, Col, Row} from 'react-bootstrap';
 import axios from 'axios';
